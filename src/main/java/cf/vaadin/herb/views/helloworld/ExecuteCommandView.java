426d52170142125030516860e3020a898fc9f090
Herberts Markuns <herberts@vaadin.com>
2026-01-19 12:53:38 +0200

    Last executed command signals

--- src/main/java/cf/vaadin/herb/views/helloworld/ExecuteCommandView.java
+++ src/main/java/cf/vaadin/herb/views/helloworld/ExecuteCommandView.java
@@ -2,9 +2,13 @@ package cf.vaadin.herb.views.helloworld;
 
 import org.vaadin.lineawesome.LineAwesomeIconUrl;
 
+import com.vaadin.flow.component.ComponentEffect;
 import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.accordion.Accordion;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.ListItem;
+import com.vaadin.flow.component.html.UnorderedList;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -12,6 +16,7 @@ import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Menu;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.signals.ListSignal;
 import com.vaadin.signals.ValueSignal;
 
 @PageTitle("Execute Command")
@@ -23,6 +28,8 @@ public class ExecuteCommandView extends VerticalLayout {
     private Div lastExecutedValue = new Div();
 
     private static final ValueSignal<String> lastExecutedCommandSignal = new ValueSignal<>("");
+    private final ListSignal<String> executedInSessionSignal = new ListSignal<>(String.class);
+    private static final ListSignal<String> executedGloballySignal = new ListSignal<>(String.class);
 
     private TextField commandField;
     private Button executeButton;
@@ -31,12 +38,15 @@ public class ExecuteCommandView extends VerticalLayout {
         commandField = new TextField("Command");
         executeButton = new Button("Execute");
         executeButton.addClickListener(e -> {
-            if (commandField.getValue().isEmpty()) {
+            var value = commandField.getValue();
+            if (value.isEmpty()) {
                 Notification.show("Please enter a command to execute.");
                 return;
             }
-            Notification.show("Executed command: " + commandField.getValue());
-            lastExecutedCommandSignal.value(commandField.getValue());
+            Notification.show("Executed command: " + value);
+            lastExecutedCommandSignal.value(value);
+            executedInSessionSignal.insertFirst(value);
+            executedGloballySignal.insertFirst(value);
         });
         executeButton.addClickShortcut(Key.ENTER);
 
@@ -46,12 +56,33 @@ public class ExecuteCommandView extends VerticalLayout {
 
         var lastExecuted = new HorizontalLayout(lastExecutedTitle, lastExecutedValue);
         lastExecutedValue.getElement().bindText(lastExecutedCommandSignal);
-    
+
+        var accordion = new Accordion();
+        var inSessionUL = new UnorderedList();
+        var globalUL = new UnorderedList();
+
+        ComponentEffect.effect(inSessionUL, () -> {
+            inSessionUL.removeAll();
+            executedInSessionSignal.value().forEach(listItem -> inSessionUL.add(new ListItem(listItem.value())));
+        });
+
+        ComponentEffect.effect(globalUL, () -> {
+            globalUL.removeAll();
+            executedGloballySignal.value().forEach(listItem -> globalUL.add(new ListItem(listItem.value())));
+        });
+
+        
+        accordion.add("Executed in this session", inSessionUL);
+        accordion.add("Executed globally", globalUL);
+
         setMargin(true);
 
         var commandLayout = new HorizontalLayout(commandField, executeButton);
         commandLayout.setAlignItems(Alignment.BASELINE);
-        add(commandLayout, lastExecuted);
+        add(commandLayout, lastExecuted, accordion);
     }
 
+
+    // Notes: 
+    // Would be nice if Unordered List had a bindlist method
 }
